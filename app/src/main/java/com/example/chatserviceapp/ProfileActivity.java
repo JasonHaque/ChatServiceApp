50a7626dc67e526523a778531ed9356332806a35
afiatamanna06 <afiatamanna06@gmail.com>
2019-10-12 12:01:32 +0600

    Profile activity modified

--- app/src/main/java/com/example/chatserviceapp/ProfileActivity.java
+++ app/src/main/java/com/example/chatserviceapp/ProfileActivity.java
@@ -1,14 +1,68 @@
 package com.example.chatserviceapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private Button logout, backToNotes;
+    private FirebaseAuth firebaseAuth;
+    private ImageView profileImage;
+    private TextView userView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        bindWidgets();
+        bindListeners();
+        userView.setText("Welcome User \n ");
+        //profileImage.setImageResource(R.drawable.profile);
+    }
+    void bindWidgets(){
+        logout=findViewById(R.id.log_out);
+        firebaseAuth=FirebaseAuth.getInstance();
+        userView=findViewById(R.id.user_view);
+        backToNotes=findViewById(R.id.notes_back_button);
+        profileImage=findViewById(R.id.profile_image);
+    }
+
+    void bindListeners(){
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(ProfileActivity.this,LogInActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            profileImage.setImageURI(selectedImage);
+        }
     }
 }
+
