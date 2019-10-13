d1bcc7cd5a01c74213db4d7c44ac6aa51ac7c4e0
jason <zahinjason220434@gmail.com>
2019-10-13 15:47:39 +0600

    fixed profile view issues

--- app/src/main/java/com/example/chatserviceapp/MainActivity.java
+++ app/src/main/java/com/example/chatserviceapp/MainActivity.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkUserStatus();
+
+    }
+
+    private void checkUserStatus(){
+        if(firebaseAuth.getCurrentUser() == null){
+            startActivity(new Intent(MainActivity.this,LogInActivity.class));
+        }
+
         startActivity(new Intent(MainActivity.this,LogInActivity.class));
     }
 }
