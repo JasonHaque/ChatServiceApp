d0552fec4dbd416212178140604d3cbc5480da3f
jason <zahinjason220434@gmail.com>
2019-10-14 21:07:38 +0600

    optimized

--- app/src/main/java/com/example/chatserviceapp/ChatViewActivity.java
+++ app/src/main/java/com/example/chatserviceapp/ChatViewActivity.java
@@ -1,27 +1,14 @@
 package com.example.chatserviceapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
-import com.bumptech.glide.Glide;
-import com.example.chatserviceapp.Model.User;
-import com.google.firebase.FirebaseAppLifecycleListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatViewActivity extends AppCompatActivity {
 
